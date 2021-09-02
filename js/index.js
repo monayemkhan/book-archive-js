27d3e0146a580af3579d877427432e23f735e0bb
Monayem Khan <monaamkhanet@gmail.com>
2021-09-02 18:18:43 +0600

    third commit check for live code

--- js/index.js
+++ js/index.js
@@ -10,38 +10,60 @@ const searchBook = () => {
     searchField.value = '';
 
     if (searchText === "") {
-        errorDiv.innerText = "Search field cannot be empty.";
+        errorDiv.innerText = "Search field cannot be empty!";
         return;
     }
 
-    const url = `http://openlibrary.org/search.json?q=${searchText}`;
+    const url = `https://openlibrary.org/search.json?q=${searchText}`;
   
     fetch(url)
     .then(res => res.json())
-    .then (data => displaySearchResult(data.docs));
+    .then((data) => {
+    // spinnner and showing data
+        setTimeout(() => {
+          spinner.classList.add("d-none");
+          displaySearchResult(data.docs);
+        },
+        1000);
+    })
+
+    .finally(() => {
+        searchField.value = "";
+    });
 
 }
+
 
 const displaySearchResult = docs => {
     const searchResult = document.getElementById('search-result');
     searchResult.textContent = '';
 
     if (docs.length === 0 ) {
-        errorDiv.innerText = "No Search Result Found";
+        errorDiv.innerText = "No Search Result Found!";
       } else {
         errorDiv.innerText = "";
-      }
+    }
 
-   docs.forEach(book => {
+    docs.forEach(book => {
+    //Author Check 
+    const isValid = validCheck => {
+        if (validCheck !== undefined) {
+            return validCheck;
+        } 
+        else {
+            return "Not Found";
+        }
+    }
+    
     const div = document.createElement('div');
     div.classList.add('col');
     div.innerHTML = `
     <div class="card h-100">
         <img height= "300px" src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top book-img" alt="...">
         <div class="card-body">
-            <h5 class="card-title">${book.title}</h5>
-            <h6 class="card-title">${book.author_name}</h6>
-            <p class="card-body fw-light">${book.first_publish_year}</p>
+            <h4 class="card-title">${book.title}</h4>
+            <h6 class="card-title">Author: ${isValid(book.author_name)}</h6>
+            <p class="card-body fw-light">First Published: ${isValid(book.first_publish_year)}</p>
         </div>
     </div>
     `;
